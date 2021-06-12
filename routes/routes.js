07e6fe57d1cf01d4487c6ea4083ffc6f227bb3c1
Gabriel Zanon <gabrielzanon77@outlook.com>
2021-06-12 13:44:13 -0300

    Create GET and POST methods

--- routes/routes.js
+++ routes/routes.js
@@ -12,8 +12,31 @@ router.use((req, res, next) => {
   console.log('Algo acontece aqui!')
   next()
 })
-router.get('/', (req, res) => {
-  res.json({ message: 'Bem Vindo!' })
+
+router.get('/showAll', async (req, res) => {
+  const client = await Client.find()
+
+  if (client.length === 0) {
+    return res.status(400).send({ error: 'No clients to show!' })
+  }
+
+  return res.send({ client })
+})
+
+router.post('/addClient', async (req, res) => {
+  const { cpf } = req.body
+
+  try {
+    if (await Client.findOne({ cpf })) {
+      return res.status(400).send({ error: 'Client already exists!' })
+    }
+
+    const client = await Client.create(req.body)
+
+    return res.send({ message: 'Client registered!' })
+  } catch (err) {
+    return res.status(400).send({ error: 'Registration failed' })
+  }
 })
 
 export default router
