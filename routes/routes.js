53f209a7764e0602e15ea1abb7f2277cf8c0bb86
Gabriel Zanon <gabrielzanon77@outlook.com>
2021-06-12 12:37:22 -0300

    Create client.js and connection Mongo in routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,10 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import Client from '../models/client.js'
 
 const router = express.Router()
+mongoose.connect('mongodb://localhost/node-crud-api',
+  { useNewUrlParser: true, useUnifiedTopology: true })
 
 router.get('/', (req, res) => {
   res.json({ message: 'Bem Vindo!' })
