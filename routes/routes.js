5a9adbf794250e715175e05b02ff0397418d21ee
Gabriel Zanon <gabrielzanon77@outlook.com>
2021-06-12 13:01:26 -0300

    Create middleware function

--- routes/routes.js
+++ routes/routes.js
@@ -3,9 +3,15 @@ import mongoose from 'mongoose'
 import Client from '../models/client.js'
 
 const router = express.Router()
-mongoose.connect('mongodb://localhost/node-crud-api',
-  { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost/node-crud-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
+router.use((req, res, next) => {
+  console.log('Algo acontece aqui!')
+  next()
+})
 router.get('/', (req, res) => {
   res.json({ message: 'Bem Vindo!' })
 })
