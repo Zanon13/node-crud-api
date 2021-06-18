962f17d09e1e6d05d9f0cf0148f5dd358ad19972
Gabriel Zanon <gabrielzanon77@outlook.com>
2021-06-17 22:35:32 -0300

    Comment code that auto increments the id field

--- models/client.js
+++ models/client.js
@@ -1,7 +1,6 @@
 import mongoose from "../database/connection.js"
-/* *** Descomente os trechos de código caso queira um _id com auto increment*** 
-   *** O método é deprecated e não achei outro jeito de resolver ***
-*/
+// *** Descomente os trechos de código caso queira um _id com auto increment *** 
+
 // import mSequence from "mongoose-sequence"
 // const AutoIncrement = mSequence(mongoose)
 
@@ -14,7 +13,7 @@ const ClientSchema = new mongoose.Schema({
     type: String,
     required: true,
   }
-})
+} /* { _id: false } */ )
 //ClientSchema.plugin(AutoIncrement)
 
 const Client = mongoose.model('Client', ClientSchema)
